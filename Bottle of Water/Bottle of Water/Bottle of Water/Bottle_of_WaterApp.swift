662d2d64635a94b140cbe59336a2eda0d5d7028d
Yiğit BAL <128126749+yigitbal5555@users.noreply.github.com>
2026-01-08 13:39:16 +0300

    First Docs.
    Uploaded main app.

---
+++ Bottle of Water/Bottle of Water/Bottle of Water/Bottle_of_WaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bottle_of_WaterApp.swift
+//  Bottle of Water
+//
+//  Created by Yiğit Bal on 8.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct Bottle_of_WaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
